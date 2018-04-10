2c00cdb39d6b698b41c6ff064865a6230e0fefe5
Misha Strilets <mishastrilets@gmail.com>
2018-04-10 21:39:41 +0300

    add form

--- src/main/webapp/WEB-INF/views/index.jsp
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -14,7 +14,6 @@
 	href="http://cdn.jsdelivr.net/webjars/bootstrap/3.3.4/css/bootstrap.min.css"
 	href="@{/webjars/bootstrap/3.3.4/css/bootstrap.min.css}"
 	rel="stylesheet" media="screen" />
-<link href="<c:url value='/static/css/style.css' />" rel="stylesheet"></link>
 
 <script type="text/javascript" src="webjars/jquery/2.2.4/jquery.min.js"></script>
 <script type="text/javascript"
@@ -22,18 +21,34 @@
 <script type="text/javascript"
 	src="https://cdnjs.cloudflare.com/ajax/libs/fabric.js/1.5.0/fabric.min.js"></script>
 <script type="text/javascript" src="/static/js/canvas.js"></script>
+<link href="<c:url value='/static/css/style.css' />" rel="stylesheet"></link>
 </head>
 
 <body>
-	<button type="button" class="btn btn-danger" onclick="addRectangle()">Add
-		rectangle</button>
-	<button type="button" class="btn btn-success" onclick="addCircle()">Add
-		circle</button>
-	<button type="button" class="btn btn-primary" onclick="addTriangle()">Add
-		triangle</button>
-	<button type="button" class="btn btn-basic" onclick="clearAll()">Clear
-		all</button>
-	<hr />
-	<canvas id="canvas"></canvas>
+	<button type="button" class="btn btn-primary" onclick="addObject()">Add
+		object</button>
+	<button type="button" class="btn btn-danger" onclick="clearAll()">Clear
+		all objects</button>
+	<button type="button" class="btn btn-basic" onclick="showAll()">Show
+		all objects</button>
+	<div class="row">
+		<div class="col-sm-9">
+			<canvas id="canvas"></canvas>
+		</div>
+		<div class="col-sm-3">
+			<form id="form">
+				<br> <label for="left">Left</label> <br> <input
+					type="text" name="left"> <br> <label for="top">Top</label>
+				<br> <input type="text" name="top"> <br> <label
+					for="width">Width object</label> <br> <input type="text"
+					name="width"> <br> <label for="height">Height
+					object</label> <br> <input type="text" name="height"> <br>
+				<label for="color">Color object</label> <br> <input type="text"
+					name="color">
+			</form>
+			<button type="button" class="btn btn-success" onclick="saveObject()">Save
+				object</button>
+		</div>
+	</div>
 </body>
 </html>
--- src/main/webapp/static/js/canvas.js
+++ src/main/webapp/static/js/canvas.js
@@ -1,44 +1,56 @@
-var canvas = activeObject = '';
+var canvas;
 
 $(document).ready(function() {
 	canvas = new fabric.Canvas('canvas', {});
-	canvas.setWidth(1300);
-	canvas.setHeight(500);
+	canvas.setWidth(1000);
+	canvas.setHeight(550);
+
+	canvas.on("object:modified", function(options) {
+		show(canvas.getActiveObject());
+	});
 });
 
-function addRectangle() {
-	var rectangle = new fabric.Rect({
+function addObject() {
+	var object = new fabric.Rect({
 		left : 50,
 		top : 50,
-		fill : 'red',
+		fill : 'black',
 		width : 50,
 		height : 50
 	});
-	canvas.add(rectangle).renderAll().setActiveObject(rectangle);
-}
-
-function addCircle() {
-	var circle = new fabric.Circle({
-		left : 50,
-		top : 50,
-		radius : 25,
-		fill : 'green',
-		stroke : 'green'
-	});
-	canvas.add(circle).renderAll().setActiveObject(circle);
-}
-
-function addTriangle() {
-	var triangle = new fabric.Triangle({
-		left : 50,
-		top : 50,
-		width : 50,
-		height : 50,
-		fill : 'blue'
-	});
-	canvas.add(triangle).renderAll().setActiveObject(triangle);
+	canvas.add(object).renderAll().setActiveObject(object);
+	show(object);
 }
 
 function clearAll() {
 	canvas.clear().renderAll();
+}
+
+function show(object) {
+	document.getElementById("form").elements["left"].value = object.left;
+	document.getElementById("form").elements["top"].value = object.top;
+	document.getElementById("form").elements["width"].value = object.width;
+	document.getElementById("form").elements["height"].value = object.height;
+	document.getElementById("form").elements["color"].value = object.fill;
+}
+
+function saveObject() {
+	var oldObject = canvas.getActiveObject();
+	canvas.remove(oldObject);
+
+	var left = document.getElementById("form").elements["left"].value;
+	var top = document.getElementById("form").elements["top"].value;
+	var width = document.getElementById("form").elements["width"].value;
+	var height = document.getElementById("form").elements["height"].value;
+	var color = document.getElementById("form").elements["color"].value;
+
+	var newObject = new fabric.Rect({
+		left : parseInt(left),
+		top : parseInt(top),
+		fill : color,
+		width : parseInt(width),
+		height : parseInt(height)
+	});
+	canvas.add(newObject).renderAll().setActiveObject(newObject);
+	show(newObject);
 }
